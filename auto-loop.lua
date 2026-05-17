c37a233e839ffffc53001a99304be8975f274ec3
szym0ne <150375668+szym0ne@users.noreply.github.com>
2026-05-17 21:30:12 +0200

    auto_loop.lua
    Refactor auto_loop function to use a constant for loop threshold and improve condition checks.

--- auto-loop.lua
+++ auto-loop.lua
@@ -1,12 +1,12 @@
+local LOOP_THRESHOLD = 15  -- loop files shorter than this (seconds)
+
 function auto_loop()
-	local file_duration = mp.get_property_number("duration", 0)
-	-- below value after string: "file_duration < " is set to 15 - it means that every file shorter than 15 sec will be infinitely looped
-	-- change this to desired limit value
-	if file_duration < 15 then
-		mp.set_property("loop-file", "yes")
-	else
-		mp.set_property("loop-file", "no")
-	end
+    local file_duration = mp.get_property_number("duration", 0)
+    if file_duration > 0 and file_duration < LOOP_THRESHOLD then
+        mp.set_property("loop-file", "yes")
+    else
+        mp.set_property("loop-file", "no")
+    end
 end
 
 mp.register_event("file-loaded", auto_loop)
